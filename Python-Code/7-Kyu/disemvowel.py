12abbc397ff6624a35f7e72cc58fd2dbb7d64adb
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2021-07-05 20:10:06 -0400

    Update disemvowel.py
    Take a string and return the string back without any vowels.

---
+++ Python-Code/7-Kyu/disemvowel.py
@@ -0,0 +1,10 @@
+def disemvowel(string):
+    vowels = ["a","e","i","o","u","A","E","I","O","U"]
+    for i in vowels:
+        string = string.replace(i,"")
+                
+    return string
+
+
+
+print(disemvowel("Hello my name is Joe."))
