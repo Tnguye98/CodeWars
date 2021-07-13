5f2c27bb1206764545a553ca1e8626277c411106
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2021-07-13 10:58:25 -0400

    Create alphanumeric.py
    Takes a string and run through a predefined rules to check if it is valid or not.

---
+++ Python-Code/6-Kyu/alphanumeric.py
@@ -0,0 +1,14 @@
+def alphanumeric(password):
+    if len(password) == 0:
+        return False
+
+    flag = 0
+    for character in password:
+        asciiValue = ord(character)
+        if 48 <= asciiValue <= 57 or 65 <= asciiValue <= 90 or 97 <= asciiValue <= 122:
+            continue
+        else:
+            flag = 1
+    if flag == 1:
+        return False
+    return True 
